62d60fc663e3298c08580c028087976b04b78998
Miguel Sandoval <miguelship3@gmail.com>
2019-05-16 21:46:44 +0200

    owner toy list and newToy features add

--- app.js
+++ app.js
@@ -4,8 +4,7 @@ const mongoose = require('mongoose');
 const hbs = require('hbs');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
-const session = require('express-session');
-const MongoStore = require('connect-mongo')(session);
+
 
 const port = 3000;
 
@@ -17,29 +16,18 @@ app.set('view engine', 'hbs');
 hbs.registerPartials(__dirname + '/views/partials');
 app.use(express.static(__dirname + '/public/'));
 
-
-app.use(session({
-    secret: 'cookie starts with c',
-    cookie: {maxAge: 60000},
-    store: new MongoStore({
-        mongooseConnection: mongoose.connection,
-        ttl: 24 * 60 * 60
-    })
-}));
-
-mongoose.connect('mongodb://localhost/toysland', {useNewUrlParse: true}, (err) => {
+mongoose.connect('mongodb://localhost/toysland', {useNewUrlParser: true}, (err) => {
     if(!err) console.log('connected');
     else console.log('Error', err);
 });
 
-app.use('/', atachUserInfo, require('./routes/toyRoutes'));
-app.use('/', atachUserInfo, require('./routes/userRoutes'));
+
+
+
+app.use('/', require('./routes/userRoutes'));
+app.use('/', require('./routes/toyRoutes'));
 app.use('/', require('./routes/orderRoutes'));
 
-function atachUserInfo(req, res, next) {
-    res.locals.currentUser = req.session.currentUser;
-    next();
-}
 
 app.listen(port, () => {
     console.log(`Listening at port: ${port}`);
